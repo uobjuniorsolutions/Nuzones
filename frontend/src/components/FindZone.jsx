97ae8fa0700d1c600c4eddb65110b8664072e552
Hugo Biais <hugobiais75@gmail.com>
2023-03-23 15:49:23 +0000

    Create components and set up routes

---
+++ frontend/src/components/FindZone.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FindZone() {
+  return (
+    <div>Find a Zone</div>
+  )
+}
+
+export default FindZone
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
+// React imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// imports for react router
+import { BrowserRouter } from 'react-router-dom'
+
+// File imports (components and styles)
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
