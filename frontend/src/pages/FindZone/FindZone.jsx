9877ad2cc1f682099fc2942b1fbdc8d24c46e076
Hugo Biais <hugobiais75@gmail.com>
2023-03-26 00:41:01 +0100

    Make Static AddZone page

--- frontend/src/pages/FindZone/FindZone.jsx
+++ frontend/src/pages/FindZone/FindZone.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './FindZone.module.css'
 
 function FindZone() {
   return (
-    <div>Find a Zone</div>
+    <div className='content'>
+      <h1 className='title'>Find a Crew</h1>
+      <input className={styles.search} placeholder="Search"/>
+      <div className={styles.map}>Place the map here</div>
+      <div className={styles.missZone}>
+        <h2>Have we missed a zone?</h2>
+        <p>Let us know where to add one!</p>
+        <button className={styles.addZone}>Add a Zone</button>
+      </div>
+    </div>
   )
 }
 
