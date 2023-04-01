b219b61012947d2d3c8b2ae1748a2e71acf01f4e
Emmanuel Abajo <temiabajo@gmail.com>
2023-04-01 13:44:42 +0100

    test build

--- src/main/java/com/nuzones/nuzonesservice/NuzonesServiceApplication.java
+++ src/main/java/com/nuzones/nuzonesservice/NuzonesServiceApplication.java
@@ -2,12 +2,19 @@ package com.nuzones.nuzonesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NuzonesServiceApplication {
+public class NuzonesServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NuzonesServiceApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NuzonesServiceApplication.class);
 	}
 
 }
