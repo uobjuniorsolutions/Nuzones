885c11d8c6cd8d9ff3c6978f571baec8e2f25ff8
Emmanuel Abajo <temiabajo@gmail.com>
2023-04-03 16:07:33 +0100

    update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS ZONES (
-    id   BIGINT NOT NULL AUTO_INCREMENT,
+    id   BIGINT NOT NULL,
     latitude DECIMAL(10,4) NOT NULL,
     longitude DECIMAL(10,4) NOT NULL,
     group_link VARCHAR(255) NOT NULL,
     image_url VARCHAR(255) NOT NULL,
     title VARCHAR(255) NOT NULL,
-    description LONGTEXT NOT NULL,
+    description VARCHAR(255) NOT NULL,
     rating INT DEFAULT 0,
     PRIMARY KEY (id)
 );
