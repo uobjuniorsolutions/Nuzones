e39cf8575663d0ce0c1cbe4a73a93a10061d697d
Hugo Biais <hugobiais75@gmail.com>
2023-04-10 20:24:11 +0200

    Modify Maps.jsx to make it work

--- frontend/src/pages/FindZone/Maps.jsx
+++ frontend/src/pages/FindZone/Maps.jsx
@@ -12,8 +12,73 @@ const center = {
     lng: -1.389
 }
 
+const tempZones = {
+    "content": [
+      {
+        "id": 1,
+        "latitude": 14.0012,
+        "longitude": 32.0844,
+        "title": "Birmingham Nuzone",
+        "groupLink": "https://fb.com/groups",
+        "imageUrl": "https://awscdn.com/image_1",
+        "description": "You are welcome to cycle in the west midlands",
+        "rating": 0
+      },
+      {
+        "id": 2,
+        "latitude": 15.0012,
+        "longitude": 31.0844,
+        "title": "London Nuzone",
+        "groupLink": "https://fb.com/groups",
+        "imageUrl": "https://awscdn.com/image_2",
+        "description": "You are welcome to cycle in the London",
+        "rating": 3
+      },
+      {
+        "id": 3,
+        "latitude": 10.0011,
+        "longitude": 23.0844,
+        "title": "Glasgow Nuzone",
+        "groupLink": "https://fb.com/groups",
+        "imageUrl": "https://awscdn.com/image_3",
+        "description": "You are welcome to cycle in the Glasgow",
+        "rating": 1
+      },
+      {
+        "id": 4,
+        "latitude": 18.0012,
+        "longitude": 34.0844,
+        "title": "Dublin Nuzone",
+        "groupLink": "https://fb.com/groups",
+        "imageUrl": "https://awscdn.com/image_4",
+        "description": "You are welcome to cycle in Dublin",
+        "rating": 2
+      },
+      {
+        "id": 5,
+        "latitude": 20,
+        "longitude": 31.0844,
+        "title": "Manchester Nuzone",
+        "groupLink": "https://fb.com/groups",
+        "imageUrl": "https://awscdn.com/image_5",
+        "description": "You are welcome to cycle in Manchester",
+        "rating": 4
+      },
+      {
+        "id": 6,
+        "latitude": 12.0003,
+        "longitude": 34.01,
+        "title": "Birmingham zone 2",
+        "groupLink": "https://fb.groups.com/brum",
+        "imageUrl": "https://cdn.images.com/ddfd-894379-csdfi4",
+        "description": "mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi",
+        "rating": 4
+      }
+    ]
+  }
+
 // For testing purposes
-const tempZones = async () => {
+const getTempZones = async () => {
     let response = await fetch('localhost:8080/api/v1/zones')
     let data = await response.json()
     console.log('DATA:', data)
