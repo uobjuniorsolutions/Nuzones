e8ad6dad9954a55d718dc599716e31527b872741
Emmanuel Abajo <temiabajo@gmail.com>
2023-04-11 17:41:11 +0100

    made a few updates

--- src/main/java/com/nuzones/nuzonesservice/dto/request/ZoneCreationDto.java
+++ src/main/java/com/nuzones/nuzonesservice/dto/request/ZoneCreationDto.java
@@ -1,18 +1,15 @@
 package com.nuzones.nuzonesservice.dto.request;
 
-import lombok.Data;
-
 /**
  * @author Emmanuel Abajo
  * @created 03/04/2023
  */
-@Data
-public class ZoneCreationDto {
-    private double latitude;
-    private double longitude;
-    private String title;
-    private String groupLink;
-    private String imageUrl;
-    private String description;
-    private int rating;
-}
+public record ZoneCreationDto (
+    double latitude,
+    double longitude,
+    String title,
+    String groupLink,
+    String imageUrl,
+    String description,
+    int rating
+) {}
--- src/main/java/com/nuzones/nuzonesservice/dto/response/ZoneCreationResponseDto.java
+++ src/main/java/com/nuzones/nuzonesservice/dto/response/ZoneCreationResponseDto.java
@@ -1,15 +1,13 @@
 package com.nuzones.nuzonesservice.dto.response;
 
 import lombok.Builder;
-import lombok.Data;
 
 /**
  * @author Emmanuel Abajo
  * @created 03/04/2023
  */
 @Builder
-@Data
-public class ZoneCreationResponseDto {
-    private Long id;
-    private String message;
-}
+public record ZoneCreationResponseDto (
+    Long id,
+    String message
+){}
